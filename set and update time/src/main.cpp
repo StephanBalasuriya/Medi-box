72bbb7607cbd18d53cf8fa5ab206aa294b57060f
StephanBalasuriya <kanishkabalasuriya2002@gamil.com>
2025-03-29 13:46:24 +0530

    set nd update time finish

--- set and update time/src/main.cpp
+++ set and update time/src/main.cpp
@@ -12,6 +12,20 @@
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// functions Declaration
+void print_line(String text, int column, int row, int text_size);
+void print_time_now();
+void update_time();
+
+// Global Variables
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+int days = 0;
+
+unsigned long timeNow = 0;
+unsigned long timeLast = 0;
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -33,17 +47,68 @@ void setup()
   // Clear the buffer
   display.clearDisplay();
 
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0); //(Column , row)
-  display.println(F("Welcome to Medibox"));
-
-  display.display();
-  delay(2000);
+  print_line(" Welcome\n    to\n  Medibox", 10, 10, 2);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  delay(10); // this speeds up the simulation
+  update_time();
+  display.clearDisplay();
+  print_time_now();
+}
+
+void print_line(String text, int column, int row, int text_size)
+{
+  display.clearDisplay();
+
+  display.setTextSize(text_size);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(column, row); //(Column , row)
+  display.println(text);
+  display.display();
+  delay(2000);
+}
+
+void print_time_now()
+{
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0); //(Column , row)
+  display.print("Time Now: ");
+  display.setTextSize(2);
+  display.setCursor(0, 20); //(Column , row)
+  display.print(days);
+  display.print(":");
+  display.print(hours);
+  display.print(":");
+  display.print(minutes);
+  display.print(":");
+  display.print(seconds);
+  display.display();
+}
+
+void update_time()
+{
+  timeNow = millis();
+  if (timeNow - timeLast > 1000)
+  {
+    timeLast = timeNow;
+    seconds++;
+    if (seconds > 59)
+    {
+      seconds = 0;
+      minutes++;
+      if (minutes > 59)
+      {
+        minutes = 0;
+        hours++;
+        if (hours > 23)
+        {
+          hours = 0;
+          days++;
+        }
+      }
+    }
+  }
 }
